89e0ded51155e10eb9bc27699891bc3c531fa12a
ToshihiroGoto <mr.densyobato@gmail.com>
2022-06-13 21:27:37 +0900

    Set Project

---
+++ ChartsSample/ChartsSample/ChartsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSampleApp.swift
+//  ChartsSample
+//
+//  Created by Toshihiro Goto on 2022/06/13.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
